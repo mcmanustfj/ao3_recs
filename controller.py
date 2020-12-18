b27f6de17a4d728a3f420d7f1f8e3b5421879e2b
Thomas McManus <mcmanustfj@gmail.com>
2020-12-18 17:22:47 -0500

    rec alg and flask app working

--- controller.py
+++ controller.py
@@ -3,8 +3,11 @@ from tempfile import mkdtemp
 from flask_session import Session
 import numpy as np
 import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+import networkx as nx
+from recomendation_engine.predictor import get_recs
 
-from recomendation_engine.predictor import get_top_rec
+
 app = Flask(__name__)
 
 app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -13,19 +16,18 @@ app.config["SESSION_TYPE"] = "filesystem"
 
 Session(app)
 
-df = pd.DataFrame({'name': ["fic1", "fic2", "fic3"],
-                   'url': ["url1", "url2", "url3"],
-                   'percent_of_users_who_liked_it': [0.3, 0.2, 0.1]})
 
 @app.route('/')
 def my_form():
     if "rec" not in session:
         session["rec"] = None
-    return render_template('view.html', rec=session["rec"], tables=[df.to_html(classes='data')], titles=df.columns.values)
+    return render_template('view.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
 	session["fic_name"]=request.form['fic_name']
-	rec=get_top_rec(session["fic_name"])
-	session["rec"] = rec
-	return render_template('view.html', fic_name=session["fic_name"], rec=session["rec"],tables=[df.to_html(classes='data')], titles=df.columns.values)
+	rec_df=get_recs(session["fic_name"])
+	session["recs"] = rec_df
+	return render_template('view.html', fic_name=session["fic_name"],tables=[rec_df.to_html(classes='data', escape=False)],titles=rec_df.columns)
+if __name__ == '__main__':
+    app.run()
--- recomendation_engine/predictor.py
+++ recomendation_engine/predictor.py
@@ -1,3 +1,14 @@
+import numpy as np
+from networkx.classes.function import common_neighbors
+import csv
+import networkx as nx
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+import pandas as pd 
+fics=np.load("data/fics.npy")
+g=nx.read_gpickle("data/g.gpickle")
+
 def get_top_rec(fic):
 	'''
 	given the title of a fic, return the title of the fic I think you would most enjoy
@@ -10,3 +21,18 @@ def get_top_rec(fic):
 		return "honestly idk, your taste is bad and im not pandering to you"
 	return "fic name not recognized"
 
+def get_recs(target,n=10):
+
+	cov=np.zeros(len(fics))
+	for i in range(0,len(fics)):
+		if not target== fics[i]:
+			cov[i]=sum(2 for i in common_neighbors(g, fics[i], target))/(len(g[fics[i]])+len(g[target]))
+	top_n_fics=fics[np.flip(np.argsort(cov))[0:10]]
+	top_n_scores=cov[np.flip(np.argsort(cov))[0:10]]
+	top_n_titles=[]
+	for fic in top_n_fics:
+	    top_n_titles.append('<a href="https://archiveofourown.org/works/'+fic+'">'+fic+'</a>')
+
+	df = pd.DataFrame(list(zip(top_n_titles,top_n_scores.tolist() )), columns =['title', 'score']) 
+
+	return df
